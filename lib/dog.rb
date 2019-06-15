6b80dc3b2b285dd29bb96bf54261ee78df4bdf50
Brian Salemi <briandsalemi@gmail.com>
2019-06-15 22:07:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,8 +34,8 @@ class Dog
    new_dog
  end
 
- def self.create(hash)
-   new_dog = Dog.new(hash)
+ def self.create(name:, breed:)
+   new_dog = Dog.new(name: name, breed: breed)
    new_dog.save
    new_dog
  end
@@ -50,7 +50,7 @@ class Dog
    new_dog = Dog.new(id: dog[0], name: dog[1], breed: dog[2])
  end
 
- def self.find_or_create_by(hash)
+ def self.find_or_create_by(name:, breed:)
    sql = <<-SQL
    SELECT * FROM dogs
    WHERE name = ? AND breed = ?
@@ -60,9 +60,29 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      dog = self.create(hash)
+      dog = self.create(name: name, breed: breed)
     end
     dog
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    dog = Dog.new(id: id, name: name, breed: breed)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    row = DB[:conn].execute(sql, name)[0]
+    dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+
+  def update
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 end
